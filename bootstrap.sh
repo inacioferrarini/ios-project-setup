aaca4c52eac32a435b63bd881031b88d07460950
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2017-07-22 19:50:43 -0300

    updated gemfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -50,11 +50,13 @@ gem install bundler
 if [ ! -f Gemfile ]; then
 echo "source \"https://rubygems.org\"
 
-ruby '$RUBY_VERSION'
-gem 'cocoapods', '1.0.1'
-gem 'slather'
+ruby '2.3.3'
+gem 'cocoapods', '~> 1.2.1'
+gem 'slather', '2.4.2'
 gem 'rake'
-gem 'xcodeproj'" >> Gemfile
+gem 'xcodeproj'
+gem 'jazzy', '0.8.3'
+gem 'xcpretty'" >> Gemfile
 fi 
 
 bundle install
